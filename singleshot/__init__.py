2b5ae45dfa7826fc90a8b9e999fc05f5124f1216
Josias Pereira <josias.ngpereira@gmail.com>
2023-08-23 09:45:15 -0300

    commit

--- singleshot/__init__.py
+++ singleshot/__init__.py
@@ -1,7 +1,25 @@
 import datetime
 import logging
+from api_in import produto_plano_de_contas
+import os
+import requests
 
 import azure.functions as func
+
+obj3 = "produto_plano_de_contas"
+url_base = os.getenv('DB_UR')
+cont_pg = 0
+url_tg = f"{url_base}/{obj3}?cursor={cont_pg}"
+def verificar_API(dados):
+    
+    if dados is not None:
+        if len(dados) > 0:
+            for estrutura in dados:
+                print(estrutura)
+        else:
+            print("Nenhum dado encontrado.")
+    else:
+        print("Falha ao acessar a API.")
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -10,5 +28,13 @@ def main(mytimer: func.TimerRequest) -> None:
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
+        print("Timer atrasado")
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    print("Executado com sucesso")
+
+produto_plano_de_contas()
+response = requests.get(url_tg)
+response.encoding = 'utf-8'  # Definir a codificação como UTF-8
+
+dados = produto_plano_de_contas(url_tg)
